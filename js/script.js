17b248cfa411fd517c63dfeee04b626c66cafe15
Vusal Gasimov <68953801+lVUSALl@users.noreply.github.com>
2022-09-16 23:41:52 +0300

    All Correct But Design Not Found

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function p_play(){
+    var x = document.getElementById("myAudio2")
+    x.pause()
+    var x = document.getElementById("myAudio")
+    x.play()
+}
+
+function p_ause(){
+    var x = document.getElementById("myAudio")
+    x.pause()
+    var x = document.getElementById("myAudio2")
+    x.pause()
+}
+
+function play_venom(){
+    var x = document.getElementById("myAudio")
+    x.pause()
+    var x = document.getElementById("myAudio2")
+    x.play()
+}
